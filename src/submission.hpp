c9656f96594ae3bb9a7ae7e425b14f4fe5743d84
Robert Lu <robertcxlee@gmail.com>
2026-09-14 16:45:11 -0400

    naive functional solution
    Local Score: ~0.425

--- src/submission.hpp
+++ src/submission.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdlib>
+#include <cstring>
 
 // Starter Grid for the 2D heat-diffusion problem.
 //
@@ -9,16 +11,76 @@
 // everything else is yours.
 class Grid {
 private:
-  std::size_t rows_;
-  std::size_t cols_;
+  size_t rows_;
+  size_t cols_;
+  size_t size_;
+
+  double* data_;
 
 public:
-  Grid(std::size_t rows, std::size_t cols);
+  Grid(size_t rows, size_t cols) {
+    rows_ = rows;
+    cols_ = cols;
 
-  double& operator()(std::size_t i, std::size_t j);
-  double  operator()(std::size_t i, std::size_t j) const;
+    size_ = rows * cols * sizeof(double);
+    data_ = (double*)malloc(size_);
+    memset(data_, 0, size_);
+  }
+
+  ~Grid() {
+    free(data_);
+  }
+
+  Grid(const Grid& other) {
+    rows_ = other.rows_;
+    cols_ = other.cols_;
+    size_ = other.size_;
+    
+    data_ = (double*)malloc(size_);
+    memcpy(data_, other.data_, size_);
+  }
+
+  Grid& operator=(const Grid& other) {
+    free(data_);
+    rows_ = other.rows_;
+    cols_ = other.cols_;
+    size_ = other.size_;
+    
+    data_ = (double*)malloc(size_);
+    memcpy(data_, other.data_, size_);
+    return *this;
+  }
+
+  size_t get_rows() const {
+    return rows_;
+  }
+
+  size_t get_cols() const {
+    return cols_;
+  }
+
+  double& operator()(size_t row, size_t col) {
+    size_t i = row * cols_ + col; 
+    return data_[i];
+  }
+
+  double  operator()(size_t row, size_t col) const {
+    size_t i = row * cols_ + col;
+    return data_[i];
+  }
 };  
 
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid. Implement your solution here.
-void apply_stencil(const Grid& old_grid, Grid& new_grid);
+void apply_stencil(const Grid& old, Grid& res) {
+  size_t n = old.get_rows();
+  size_t m = old.get_cols();
+
+  res = old;
+  for (int i = 1; i < n - 1; i++) {
+    for (int j = 1; j < m - 1; j++) {
+      res(i, j) = 0.5 * old(i, j);
+      res(i, j) += 0.125 * (old(i + 1, j) + old(i - 1, j) + old(i, j + 1) + old(i, j - 1));
+    }
+  }
+}
