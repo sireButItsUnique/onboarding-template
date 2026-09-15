2824a35097e74413e885d9a751bca9e8c38b9afb
Robert Lu <robertcxlee@gmail.com>
2026-09-15 10:34:47 -0400

    added avx
    Local Score: ~0.565

--- src/submission.hpp
+++ src/submission.hpp
@@ -3,6 +3,8 @@
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
+#include <iostream>
+#include <immintrin.h>
 
 // Starter Grid for the 2D heat-diffusion problem.
 //
@@ -31,15 +33,6 @@ public:
     free(data_);
   }
 
-  Grid(const Grid& other) {
-    rows_ = other.rows_;
-    cols_ = other.cols_;
-    size_ = other.size_;
-    
-    data_ = (double*)malloc(size_);
-    memcpy(data_, other.data_, size_);
-  }
-
   Grid& operator=(const Grid& other) {
     free(data_);
     rows_ = other.rows_;
@@ -58,6 +51,14 @@ public:
   size_t get_cols() const {
     return cols_;
   }
+  
+  double* row_ptr(size_t i) {
+    return data_ + i * cols_; 
+  }
+  
+  const double* row_ptr(size_t i) const { 
+    return data_ + i * cols_; 
+  }
 
   double& operator()(size_t row, size_t col) {
     size_t i = row * cols_ + col; 
@@ -72,15 +73,40 @@ public:
 
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid. Implement your solution here.
+__attribute__((target("avx2,fma")))
 void apply_stencil(const Grid& old, Grid& res) {
   size_t n = old.get_rows();
   size_t m = old.get_cols();
 
   res = old;
-  for (int i = 1; i < n - 1; i++) {
-    for (int j = 1; j < m - 1; j++) {
-      res(i, j) = 0.5 * old(i, j);
-      res(i, j) += 0.125 * (old(i + 1, j) + old(i - 1, j) + old(i, j + 1) + old(i, j - 1));
+
+  if (n < 3 || m < 3) return; // no interior points to update
+
+  const __m256d half = _mm256_set1_pd(0.5);
+  const __m256d eighth = _mm256_set1_pd(0.125);
+
+  for (size_t i = 1; i < n - 1; i++) {
+    const double* c = old.row_ptr(i);
+    const double* up = old.row_ptr(i - 1);
+    const double* down = old.row_ptr(i + 1);
+    double* dst = res.row_ptr(i);
+
+    size_t j = 1;
+    for (; j + 4 <= m - 1; j += 4) { // run 4 at a time for interior points only
+      __m256d vc = _mm256_loadu_pd(c + j);
+      __m256d vup = _mm256_loadu_pd(up + j);
+      __m256d vdown = _mm256_loadu_pd(down + j);
+      __m256d vleft = _mm256_loadu_pd(c + j - 1);
+      __m256d vright = _mm256_loadu_pd(c + j + 1);
+
+      __m256d neighbors = _mm256_add_pd(_mm256_add_pd(vup, vdown), _mm256_add_pd(vleft, vright));
+      __m256d r = _mm256_add_pd(_mm256_mul_pd(half, vc), _mm256_mul_pd(eighth, neighbors));
+      _mm256_storeu_pd(dst + j, r);
+    }
+
+    // scalar for leftover
+    for (; j < m - 1; j++) {
+      dst[j] = 0.5 * c[j] + 0.125 * (up[j] + down[j] + c[j - 1] + c[j + 1]);
     }
   }
 }
